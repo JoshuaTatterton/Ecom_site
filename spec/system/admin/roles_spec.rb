583bab4cf2f957aea3b855adcddf4cc490b8817d
Josh Tatterton <Tattsy@hotmail.co.uk>
2025-11-11 21:13:30 +0000

    Add basic edit page

--- spec/system/admin/roles_spec.rb
+++ spec/system/admin/roles_spec.rb
@@ -13,9 +13,11 @@ RSpec.describe "Roles Admin", type: :system do
         within("tbody#roles") do
           within("tr:nth-child(1)") do
             expect(page).to have_content(sweet_role.name)
+            expect(page).to have_selector("a[href='#{edit_admin_role_path(Switch.current_account, sweet_role.id)}']")
           end
           within("tr:nth-child(2)") do
             expect(page).to have_content(admin_role.name)
+            expect(page).not_to have_selector("a[href='#{edit_admin_role_path(Switch.current_account, admin_role.id)}']")
           end
         end
         expect(page).to have_selector("a[href='#{new_admin_role_path("primary")}']")
@@ -25,8 +27,6 @@ RSpec.describe "Roles Admin", type: :system do
 
   describe "#create" do
     scenario "creating a role with all permissions" do
-      # Arrange
-
       # Act
       visit new_admin_role_path(Switch.current_account)
 
@@ -37,6 +37,27 @@ RSpec.describe "Roles Admin", type: :system do
 
       # Assert
       expect(page).to have_content("Sweet Role")
+    end
+  end
+
+  describe "#update" do
+    scenario "updating a role" do
+      # Arrange
+      role = Role.create(name: "AHHHH")
+
+      # Act
+      visit edit_admin_role_path(Switch.current_account, role.id)
+
+      within("form[action='#{admin_role_path(Switch.current_account, role.id)}']") do
+        find("input[name='role[name]']").fill_in(with: "Sweet Role")
+        find("input[type='submit']").click
+      end
+
+      # Assert
+      aggregate_failures do
+        expect(page).to have_content("Sweet Role")
+        expect(role.reload.name).to eq("Sweet Role")
+      end
     end
   end
 end
