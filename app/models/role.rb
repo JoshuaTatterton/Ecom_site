122df573bb8b9f9a0971e23a3f0282933d2cb22a
Josh Tatterton <Tattsy@hotmail.co.uk>
2025-11-13 18:45:14 +0000

    Prevent role updates when an admin role

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,6 +6,22 @@ class Role < ApplicationRecord
   has_many :memberships
   has_many :users, through: :memberships
 
+  attr_readonly :administrator
+
   validates :name, presence: true, uniqueness: { scope: :account_reference }
   validates :permissions, json: { schema: PERMISSIONS_SCHEMA }
+  validate :cannot_update_administrator_role, if: :administrator, on: :update
+
+  before_destroy :prevent_administrator_role_destruction, if: :administrator
+
+  private
+
+  def cannot_update_administrator_role
+    errors.add(:administrator, :cannot_update)
+  end
+
+  def prevent_administrator_role_destruction
+    errors.add(:administrator, :cannot_destroy)
+    raise ActiveRecord::RecordNotDestroyed
+  end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -61,5 +61,59 @@ RSpec.describe Role, type: :model do
         end
       end
     end
+
+    describe "#administrator" do
+      it "can create an admin role" do
+        # Act
+        role = Role.create(name: "Administrator", administrator: true)
+
+        # Assert
+        expect(role).to be_persisted
+      end
+
+      it "cannot change :administrator" do
+        # Arrange
+        role = Role.create(name: "Administrator", administrator: false)
+
+        # Act & Assert
+        expect {
+          role.update(administrator: true)
+        }.to raise_error(ActiveRecord::ReadonlyAttributeError)
+      end
+
+      it "cannot update an admin role" do
+        # Arrange
+        role = Role.create(name: "Administrator", administrator: true)
+
+        # Act & Assert
+        role.update(name: "AAHHHHH")
+
+        # Assert
+        aggregate_failures do
+          expect(role).to be_invalid
+          errors = role.errors.group_by_attribute
+          expect(errors[:administrator].length).to eq(1)
+          expect(errors[:administrator][0].type).to eq(:cannot_update)
+        end
+      end
+
+      it "cannot destroy an admin role" do
+        # Arrange
+        role = Role.create(name: "Administrator", administrator: true)
+
+        # Act & Assert
+        expect {
+          role.reload.destroy
+        }.to change(Role, :count).by(0)
+
+        # Assert
+        aggregate_failures do
+          expect(role).to be_invalid
+          errors = role.errors.group_by_attribute
+          expect(errors[:administrator].length).to eq(1)
+          expect(errors[:administrator][0].type).to eq(:cannot_update)
+        end
+      end
+    end
   end
 end
