92ff47d0b4bc3b502699d70f78b5da1f353e5180
Josh Tatterton <Tattsy@hotmail.co.uk>
2025-12-09 14:33:00 +0000

    Update account generator to create users and admin role

--- lib/tasks/generate_account.rake
+++ lib/tasks/generate_account.rake
@@ -3,8 +3,13 @@ namespace :account do
   task generate: :environment do
     account_reference = ENV.fetch("ACCOUNT_REFERENCE")
     account_name = ENV.fetch("ACCOUNT_NAME", nil)
+    user_email = ENV.fetch("ADMIN_USER_EMAIL")
 
-    generator = AccountGenerator.new(reference: account_reference, name: account_name)
+    generator = AccountGenerator.new(
+      reference: account_reference,
+      name: account_name,
+      email: user_email
+    )
 
     generator.call!
   end
--- spec/services/account_generator_spec.rb
+++ spec/services/account_generator_spec.rb
@@ -1,7 +1,7 @@
-RSpec.describe AccountGenerator do
+RSpec.describe AccountGenerator, :unscoped do
   it "creates the account when providing account information" do
     # Arrange
-    generator = AccountGenerator.new(reference: "ecom", name: "Ecom")
+    generator = AccountGenerator.new(reference: "ecom", name: "Ecom", email: "example@email.co.uk")
 
     # Act & Assert
     expect {
@@ -16,7 +16,7 @@ RSpec.describe AccountGenerator do
 
   it "auto-generates account name when not provided" do
     # Arrange
-    generator = AccountGenerator.new(reference: "super_duper_special_awesome_guy")
+    generator = AccountGenerator.new(reference: "super_duper_special_awesome_guy", email: "example@email.co.uk")
 
     # Act & Assert
     expect {
@@ -33,6 +33,64 @@ RSpec.describe AccountGenerator do
       # Currently set to camelize, could be titleize is wanted
       expect(account.name).to eq("SuperDuperSpecialAwesomeGuy")
       expect(account.account_setting).to be_present
+    end
+  end
+
+  it "creates an admin Role and User from provided email triggering complete sign up job" do
+    # Arrange
+    email = "my_user@email.co.uk"
+    generator = AccountGenerator.new(reference: "ecom", name: "Ecom", email: email)
+
+    # Act & Assert
+    expect {
+      expect {
+        generator.call!
+      }.not_to raise_error
+    }.to change(Role.unscoped, :count).by(1)
+    .and change(User, :count).by(1)
+
+    # Assert
+    aggregate_failures do
+      Switch.account("ecom") do
+        admin_role = Role.find_by(name: "Admin")
+        expect(admin_role.administrator).to eq(true)
+        expect(admin_role.users.count).to eq(1)
+
+        created_user = User.find_by(email: email)
+        expect(created_user.awaiting_authentication).to eq(true)
+        expect(created_user.role).to eq(admin_role)
+
+        expect(UserSignUpJob).to have_enqueued_sidekiq_job(created_user.id)
+        expect(UserSignUpJob.jobs.dig(0, "account_reference")).to eq("ecom")
+      end
+    end
+  end
+
+  it "creates an admin Role and attaches to an existing user, doesn't trigger sign up job" do
+    # Arrange
+    email = "my_user@email.co.uk"
+    user = User.create(email: email)
+    generator = AccountGenerator.new(reference: "ecom", name: "Ecom", email: email)
+
+    # Act & Assert
+    expect {
+      expect {
+        generator.call!
+      }.not_to raise_error
+    }.to change(Role.unscoped, :count).by(1)
+    .and change(User, :count).by(0)
+
+    # Assert
+    aggregate_failures do
+      Switch.account("ecom") do
+        admin_role = Role.find_by(name: "Admin")
+        expect(admin_role.administrator).to eq(true)
+        expect(admin_role.users.count).to eq(1)
+
+        expect(user.reload.role).to eq(admin_role)
+
+        expect(UserSignUpJob).not_to have_enqueued_sidekiq_job
+      end
     end
   end
 end
